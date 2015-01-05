7f65e914d49ac023402420d79b3341768a0a8f74
E.L <eladlavi@Elads-MacBook-Pro.local>
2015-01-05 21:22:18 +0200

    a

---
+++ LocationLogging/app/src/main/java/com/example/locationlogging/DBAdapter.java
@@ -0,0 +1,74 @@
+package com.example.locationlogging;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+/**
+ * Created by eladlavi on 1/5/15.
+ * http://www.sqlite.org/lang.html
+ * http://sqlitebrowser.org/
+ */
+public class DBAdapter {
+    static final String KEY_DATE = "SampleTime";
+    static final String KEY_LAT = "Lat";
+    static final String KEY_LNG = "Lng";
+    static final String DATABASE_NAME = "MyDB";
+    static final String DATABASE_TABLE = "Locations";
+    static final int DATABASE_VERSION = 1;
+
+    final Context context;
+    SQLiteDatabase db;
+    DatabaseHelper dbHelper;
+
+    public DBAdapter(Context ctx){
+        this.context = ctx;
+        dbHelper = new DatabaseHelper(ctx);
+    }
+
+    private static class DatabaseHelper extends SQLiteOpenHelper{
+
+        public DatabaseHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL("CREATE TABLE " + DATABASE_TABLE + "("+KEY_DATE+" INTEGER, "+KEY_LAT+" REAL, "+KEY_LNG+" REAL);");
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("DROP TABLE IF EXISTS " + DATABASE_TABLE);
+            onCreate(db);
+        }
+    }
+
+    public void open() {
+        db = dbHelper.getWritableDatabase();
+    }
+
+    public void close(){
+        db.close();
+    }
+
+    //this method will add a new record to the Locations table.
+    public long insertLocation(long sampleTime, float lat, float lng){
+        ContentValues values = new ContentValues();
+        values.put(KEY_DATE, sampleTime);
+        values.put(KEY_LAT, lat);
+        values.put(KEY_LNG, lng);
+        return db.insert(DATABASE_TABLE, null, values);
+    }
+
+    public Cursor getAllLocations(){
+        return db.query(DATABASE_TABLE,new String[]{KEY_DATE, KEY_LAT,
+                KEY_LNG}, null, null, null, null, null);
+
+    }
+
+
+}
--- LocationLogging/app/src/main/java/com/example/locationlogging/MainActivity.java
+++ LocationLogging/app/src/main/java/com/example/locationlogging/MainActivity.java
@@ -1,19 +1,68 @@
 package com.example.locationlogging;
 
+import android.database.Cursor;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*
+        DBAdapter db = new DBAdapter(this);
+        db.open();
+        db.insertLocation(System.currentTimeMillis(), 123.123453f, 456.4345556f);
+        db.insertLocation(System.currentTimeMillis(), 123.113453f, 456.4341556f);
+
+        Cursor c = db.getAllLocations();
+        while(c.moveToNext()){
+            long sampleTime = c.getLong(c.getColumnIndex(DBAdapter.KEY_DATE));
+            //or simply: c.getLong(0); if we already know the column index.
+            float lat = c.getFloat(c.getColumnIndex(DBAdapter.KEY_LAT));
+            float lng = c.getFloat(c.getColumnIndex(DBAdapter.KEY_LNG));
+            Log.d("Elad","record: time: " + sampleTime + ", lat: " + lat + ", lng: " + lng);
+
+
+        }
+        c.close();
+        db.close();
+        */
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for(int i=0;i<30;i++){
+                    Log.d("Elad", "value " + i);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        t.start();
     }
 
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("Elad", "in onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("Elad", "in onDestroy");
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
